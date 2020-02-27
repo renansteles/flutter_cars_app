537d2cd13013f38ec383d3b8cfb0eea24d5c8b79
Renan Teles <renansteles@gmail.com>
2020-02-26 21:22:48 -0300

    aula94 e 95: parse de json no construtor das models

--- lib/pages/usuario.dart
+++ lib/pages/usuario.dart
@@ -1,24 +1,24 @@
 class Usuario {
-	String login;
-	String nome;
-	String email;
-	String token;
+  String login;
+  String nome;
+  String email;
+  String token;
 
-	List<String> roles;
+  List<String> roles;
 
-	//Exemplo de Named Constructor. Padrão muito utilizado no Dart
-	// Exemplo tambem de lista de inicialização, onde ao invés das {} é utilizado : para comecar e no ultimo item ;
-	Usuario.fromJson(Map<String, dynamic> map) :
-		this.login = map["login"],
-		this.nome = map["nome"],
-		this.email = map["email"],
-		this.token = map["token"];
+  //Exemplo de Named Constructor. Padrão muito utilizado no Dart
+  // Exemplo tambem de lista de inicialização, onde ao invés das {} é utilizado : para comecar e no ultimo item ;
+  Usuario.fromJson(Map<String, dynamic> map)
+      : this.login = map["login"],
+        this.nome = map["nome"],
+        this.email = map["email"],
+        this.token = map["token"],
+        roles = map["roles"] != null
+            ? map["roles"].map<String>((role) => role.toString()).toList()
+            : null;
 
-
-	@override
+  @override
   String toString() {
     return "Usuário toString: login: $login , nome $nome , email $email, token $token, roles $roles";
   }
-
-
 }

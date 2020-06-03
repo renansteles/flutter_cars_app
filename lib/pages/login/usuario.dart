81aa752907562c66ad26abc1079fb4b502f10ab9
Renan Teles <renansteles@gmail.com>
2020-06-03 19:31:13 -0300

    aula 117: Salvando dados do usuario logado no shared_preferences

--- lib/pages/login/usuario.dart
+++ lib/pages/login/usuario.dart
@@ -1,24 +1,62 @@
+import 'dart:io';
+
+import 'package:carros/utils/prefs.dart';
+import 'dart:convert' as convert;
+
 class Usuario {
+  int id;
   String login;
   String nome;
   String email;
+  String urlFoto;
   String token;
-
   List<String> roles;
 
-  //Exemplo de Named Constructor. Padrão muito utilizado no Dart
-  // Exemplo tambem de lista de inicialização, onde ao invés das {} é utilizado : para comecar e no ultimo item ;
-  Usuario.fromJson(Map<String, dynamic> map)
-      : this.login = map["login"],
-        this.nome = map["nome"],
-        this.email = map["email"],
-        this.token = map["token"],
-        roles = map["roles"] != null
-            ? map["roles"].map<String>((role) => role.toString()).toList()
-            : null;
+  Usuario(
+      {this.id,
+      this.login,
+      this.nome,
+      this.email,
+      this.urlFoto,
+      this.token,
+      this.roles});
+
+  Usuario.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    login = json['login'];
+    nome = json['nome'];
+    email = json['email'];
+    urlFoto = json['urlFoto'];
+    token = json['token'];
+    roles = json['roles'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['login'] = this.login;
+    data['nome'] = this.nome;
+    data['email'] = this.email;
+    data['urlFoto'] = this.urlFoto;
+    data['token'] = this.token;
+    data['roles'] = this.roles;
+    return data;
+  }
+
+  void save() {
+    Map map = toJson();
+    String json = convert.json.encode(map);
+    Prefs.setString("user.prefs", json);
+  }
+
+  static Future<Usuario> get() async {
+    String json = await Prefs.getString("user.prefs");
+    Map map = convert.jsonDecode(json);
+    return Usuario.fromJson(map);
+  }
 
   @override
   String toString() {
-    return "Usuário toString: login: $login , nome $nome , email $email, token $token, roles $roles";
+    return 'Usuario{id: $id, login: $login, nome: $nome, email: $email, urlFoto: $urlFoto, token: $token, roles: $roles}';
   }
 }

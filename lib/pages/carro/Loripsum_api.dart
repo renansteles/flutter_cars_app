3f26a50f3fe4700abad238f3041ed58802dbe0c3
Renan Teles <renansteles@gmail.com>
2020-06-15 20:44:47 -0300

    aula 142: Detalhe de carros salvando dados na variável de cache, para prevenir novas requisicoes

--- lib/pages/carro/Loripsum_api.dart
+++ lib/pages/carro/Loripsum_api.dart
@@ -2,11 +2,14 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 class LoripsumBloc {
+  //Variavel para salvar dados em cache. Usada no if ternario ??
+  static String lorim;
   final _streamController = StreamController<String>();
   Stream<String> get stream => _streamController.stream;
   fetch() async {
     try {
-      String s = await LoripsumApi.getLoripsum();
+      //Se variavel nao é nula, retorna ela.
+      String s = lorim ?? await LoripsumApi.getLoripsum();
       _streamController.add(s);
     } on Exception catch (e) {
       _streamController.addError(e); //public a exception na stream
@@ -20,7 +23,8 @@ class LoripsumBloc {
 
 class LoripsumApi {
   static Future<String> getLoripsum() async {
-    var url = 'https://loripsum.net/api';
+//    var url = 'https://loripsum.net/api';
+	var url = 'https://baconipsum.com/api/?type=meat-and-filler';
     print("LORIPSUM_API GET> $url");
     var response = await http.get(url);
 
@@ -29,6 +33,8 @@ class LoripsumApi {
 
     text = text.replaceAll("<p>", "");
     text = text.replaceAll("</p>", "");
+	text = text.replaceAll("[", "");
+	text = text.replaceAll("]", "");
     return text;
   }
 }

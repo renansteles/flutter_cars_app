8a1ff0cfab8b261be4b111c531b941d894fe8b65
Renan Teles <renansteles@gmail.com>
2020-06-15 19:02:13 -0300

    aula 137: Projeto habilitado para web. Criado menu de popup na action bar

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -11,6 +11,35 @@ class CarroPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(carro.nome),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: _onClickMapa,
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: _onClickVideo,
+          ),
+          PopupMenuButton<String>(
+            onSelected: (String valor) => _onClickPopupMenu(valor),
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  value: "Editar",
+                  child: Text('Editar'),
+                ),
+                PopupMenuItem(
+                  value: "Deletar",
+                  child: Text('Deletar'),
+                ),
+                PopupMenuItem(
+                  value: "Compartilhar",
+                  child: Text('Compartilhar'),
+                ),
+              ];
+            },
+          ),
+        ],
       ),
       body: _body(),
     );
@@ -18,8 +47,26 @@ class CarroPage extends StatelessWidget {
 
   _body() {
     return Container(
-		padding: EdgeInsets.all(16),
+      padding: EdgeInsets.all(16),
       child: Image.network(carro.urlFoto),
     );
+  }
+
+  void _onClickMapa() {}
+
+  void _onClickVideo() {}
+
+  _onClickPopupMenu(String valor) {
+    switch (valor) {
+      case "Editar":
+        print("Editar!");
+        break;
+      case "Deletar":
+        print("Deletar!");
+        break;
+      case "Compartilhar":
+        print("Compartilhar!");
+        break;
+    }
   }
 }
